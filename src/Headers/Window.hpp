32012fe8abf07c099a2fd1ca8c09eab4fa1ac7b3
ViperEvil <shulzhenko.23@mail.ru>
2025-12-15 22:13:50 +0300

    Revert "Merge branch 'main' into NikitaBranch"
    This reverts commit 086fa7777642fe48bc04123f19c490fb7ea5ead5, reversing
    changes made to 229d1a88a70fa4585224afa871f3630d74ff11a5.

---
+++ src/Headers/Window.hpp
@@ -0,0 +1,16 @@
+//
+// Created by akemi on 09.12.2025.
+//
+
+#ifndef KGG_CPP_PROJECT_REPO_WINDOW_H
+#define KGG_CPP_PROJECT_REPO_WINDOW_H
+
+
+class Window {
+public:
+    static void create_Window();
+    static void drawcube();
+};
+
+
+#endif //KGG_CPP_PROJECT_REPO_WINDOW_H
---
+++ src/Main/Light/Normal.cpp
@@ -0,0 +1,5 @@
+//
+// Created by akemi on 08.12.25.
+//
+
+#include "../Headers/Normal.h"
